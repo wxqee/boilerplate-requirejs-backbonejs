ac93caf513ad6f4a9f4e6fb95e951acace38d80f
Wang Xiaoqiang <xq21@vip.qq.com>
2015-06-03 22:20:34 +0800

    Css loading method updating, and refactoring the whole project.

--- js/init.js
+++ js/init.js
@@ -4,34 +4,34 @@
  */
 +(function (root) {
 
-
 /* global container for none require.js modules */
-root.App = root.App || {
+var App = root.App || {
     env: 'dev'
 };
-var App = root.App;
 
 
-/* i18n works with module <views/locale-view> */
-(function() {
-    // get locale in localStorage
-    var localeRuntime = localStorage.getItem('locale') || (
+function getLocale() {
+    var locale = localStorage.getItem('locale') || (
         String.prototype.toLowerCase.call(navigator.language || navigator.userLanguage)
     );
 
     // make sure localStorage knows it, when <views/locale-view> using it later.
-    localStorage.setItem('locale', localeRuntime );
+    localStorage.setItem('locale', locale);
+    return locale;
+}
 
-    // initialize require.js on first step.
-    root.requirejs = {
-        urlArgs: "bust=" +  (new Date()).getTime(),
-        config: { i18n: { locale: localeRuntime } }
-    };
 
-    // make sure prod environment will cache resources in browser by default.
-    if (App.env === 'prod') {
-        delete root.requirejs.urlArgs;
-    }
-})();
+// initialize require.js on first step.
+root.requirejs = {
+    urlArgs: "bust=" +  (new Date()).getTime(),
+    config: { i18n: { locale: getLocale() } }
+};
+
+// make sure prod environment will cache resources in browser by default.
+if (App.env === 'prod') {
+    delete root.requirejs.urlArgs;
+}
+
+root.App = App;
 
 })(this);
